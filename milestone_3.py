b90ef515c5841f8e94da4e0dad466183f813a2c9
Janis Gulbis <gulbisj@gmail.com>
2024-11-05 10:37:46 +0000

    Checks if letter is in the word. Asks user to try again if input is invalid.

---
+++ milestone_3.py
@@ -0,0 +1,10 @@
+# %%
+
+while True:
+    guess = input("Please enter a single letter: ")
+
+    if len(guess) == 1 and guess.isalpha():
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+# %%
