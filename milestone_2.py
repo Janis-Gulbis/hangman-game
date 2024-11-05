3cc4cb9d72e089847e1118d3782a98ed80c4a35a
Janis Gulbis <gulbisj@gmail.com>
2024-11-05 09:17:24 +0000

    Refactors and optimizes code.

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,11 @@
 # %%
 import random
 
+# Initialize word list and randomly select a word
 word_list = ["apple", "banana", "orange", "strawberry", "melon"]
 word = random.choice(word_list)
 
+# Get user input and validate
 guess = input("Please enter a single letter:")
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
